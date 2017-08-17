89ba7f74c0522893f5f33c4fb23b4236f110b992
Anil <anilkumar.audacious@gmail.com>
2017-08-17 16:39:55 -0400

    Update jsdoSettings.js

--- Hierarchical - PDS Template/scripts/jsdoSettings.js
+++ Hierarchical - PDS Template/scripts/jsdoSettings.js
@@ -1,12 +1,12 @@
 
 var jsdoSettings = { 
-      "serviceURI": "http://oemobiledemo.progress.com/CustOrderService",
-      "catalogURIs": "http://oemobiledemo.progress.com/CustOrderService/static/CustOrderSubService.json",     
+      "serviceURI": "http://oemobiledemo.progress.com/OEMobileDemoServices",
+      "catalogURIs": "http://oemobiledemo.progress.com/OEMobileDemoServices/static/SportsService.json",     
       "authenticationModel": "Anonymous",
-      "resourceName": "CustOrderNSub",
-      "parentTableName": "eCustomer",	//Please note that this tableName is being used by tableRef property when the 
+      "resourceName": "CustomerOrders",
+      "parentTableName": "ttCustomer",	//Please note that this tableName is being used by tableRef property when the 
     						         //resource is built on top of two tables. Say Customer and Order    
-      "childTableName": "eOrder",
+      "childTableName": "ttOrder",
       "parentDisplayFields": "CustNum,Name",
       "childDisplayFields": "OrderNum, OrderStatus"
 };
